43dbca78e73d2384b85f1c3d61c30b8a5c25ff6d
EC2 Default User <ec2-user@ip-172-31-30-111.us-west-2.compute.internal>
2020-04-04 05:21:01 +0000

    Modifying files

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,21 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=ada2ac014946
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl run kalpro5\
+    --generator=run-pod/v1\
+    --image=$dockerpath\
+    --port=80 --labels app=kalpro5
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
-# Forward the container port to a host
+# Forward the container port to a 
+kubectl port-forward kalpro5 8000:80
 
--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=ada2ac014946
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login --username=kunvar13
+docker tag $dockerpath kunvar13/kalpro5:project
 
 # Step 3:
 # Push image to a docker repository
+docker push kunvar13/kalpro5:project
